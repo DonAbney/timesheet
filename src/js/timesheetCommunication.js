5ec156099065e4e5e14f3002e43ad5ff7d3b42ff
Michael Cibulskis <mcibulskis@pillartechnology.com>
2016-06-10 23:54:16 -0400

    adding application identifier request header

--- src/js/timesheetCommunication.js
+++ src/js/timesheetCommunication.js
@@ -1,13 +1,16 @@
 var TimesheetCommunication = (function() {
   var self = {
-    // Update with prod URL
-    url_getTimesheetForUser: "http://private-696ecf-ddaugherfba.apiary-mock.com/api/timesheet"
+    requestHeaders: {
+      'Application-Identifier': "github.com/donabney/timesheet"
+    },
+    url_getTimesheetForUser: "http://private-696ecf-ddaugherfba.apiary-mock.com/api/timesheet" // Update with prod URL
   };
 
   self.fetchTimesheetInfo = function(username, date) {
     $.ajax({
       url: self.url_getTimesheetForUser + '/' + username + '/' + TimesheetUtil.formatDateYYYYMMDD(date),
-      crossDomain: true
+      crossDomain: true,
+      headers: self.requestHeaders
     }).then(function(data) {
       TimesheetView.displayTimesheetInfo(username, data);
     });
