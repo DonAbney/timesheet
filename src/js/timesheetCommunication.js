3e74076784ba8e54aa9a80b22fc3308710d86388
Michael Cibulskis <mcibulskis@pillartechnology.com>
2016-06-11 23:17:56 -0400

    adjusted handling of mocking error responses via apiary for manual testing, added handling of error response

--- src/js/timesheetCommunication.js
+++ src/js/timesheetCommunication.js
@@ -1,13 +1,11 @@
 var TimesheetCommunication = (function() {
   var self = {
     requestHeaders: {
+      // Prefer:status=404,  // This is for mocked error responses with apiary to assist with manual testing
       'Application-Identifier': "github.com/donabney/timesheet"
     },
-    url_getTimesheetForUser: "http://private-696ecf-ddaugherfba.apiary-mock.com/api/timesheet" // Update with prod URL
-
-    // Mocked error endpoints for manual testing of error responses
-    // url_getTimesheetForUser: "http://private-f9346-errorstates.apiary-mock.com/404"
-    // url_getTimesheetForUser: "http://private-f9346-errorstates.apiary-mock.com/500"
+    url_getTimesheetForUser: "http://private-696ecf-ddaugherfba.apiary-mock.com/api/timesheet" // apiary mock response url
+    // url_getTimesheetForUser: "" // prod url
   };
 
   self.fetchTimesheetInfo = function(username, date) {
@@ -15,8 +13,12 @@ var TimesheetCommunication = (function() {
       url: self.url_getTimesheetForUser + '/' + username + '/' + TimesheetUtil.formatDateYYYYMMDD(date),
       crossDomain: true,
       headers: self.requestHeaders
-    }).then(function(data) {
+    }).done(function(data) {
+      ErrorHandling.makeSuccessResponseVisible();
       TimesheetView.displayTimesheetInfo(username, data);
+    }).fail(function(jqXHR, statusText, errorThrown) {
+      ErrorHandling.makeErrorResponseVisible();
+      ErrorHandling.displayError("[" + jqXHR.status + "] " + jqXHR.statusText, JSON.parse(jqXHR.responseText).message);
     });
   };
 
