4e9bab05676a565a3b6517c4c63782aed1c9d471
Michael Cibulskis <mcibulskis@pillartechnology.com>
2016-06-11 23:36:52 -0400

    refactoring

--- src/js/timesheetCommunication.js
+++ src/js/timesheetCommunication.js
@@ -1,16 +1,25 @@
 var TimesheetCommunication = (function() {
   var self = {
     requestHeaders: {
-      // Prefer:status=404,  // This is for mocked error responses with apiary to assist with manual testing
       'Application-Identifier': "github.com/donabney/timesheet"
-    },
-    url_getTimesheetForUser: "http://private-696ecf-ddaugherfba.apiary-mock.com/api/timesheet" // apiary mock response url
-    // url_getTimesheetForUser: "" // prod url
+    }
   };
+
+  // Apiary mock URLs
+  self.url = {
+    getTimesheetForUser: "http://private-696ecf-ddaugherfba.apiary-mock.com/api/timesheet"
+  };
+  // self.requestHeaders.Prefer = "status=404"  // This is for mocked error responses with apiary to assist with manual testing
+
+
+  // Prod URLs
+  // self.url = {
+  //   getTimesheetForUser: ""
+  // };
 
   self.fetchTimesheetInfo = function(username, date) {
     $.ajax({
-      url: self.url_getTimesheetForUser + '/' + username + '/' + TimesheetUtil.formatDateYYYYMMDD(date),
+      url: self.url.getTimesheetForUser + '/' + username + '/' + TimesheetUtil.formatDateYYYYMMDD(date),
       crossDomain: true,
       headers: self.requestHeaders
     }).done(function(data) {
