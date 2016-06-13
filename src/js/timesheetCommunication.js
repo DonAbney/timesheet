7f0d34e2b5f952a988062f60032e7db660d88042
Michael Cibulskis <mcibulskis@pillartechnology.com>
2016-06-12 21:12:14 -0400

    refactoring url generation, adding stubs for save and validate requests

--- src/js/timesheetCommunication.js
+++ src/js/timesheetCommunication.js
@@ -2,24 +2,38 @@ var TimesheetCommunication = (function() {
   var self = {
     requestHeaders: {
       'Application-Identifier': "github.com/donabney/timesheet"
+    },
+    protocol: "http",
+    host: "private-696ecf-ddaugherfba.apiary-mock.com",
+    api: {
+      getTimesheetForUser: "/api/timesheet/{email_shortname}/{date}",
+      saveTimesheet: "/api/timesheet/{id}",
+      validateTimesheet: "/api/timesheet/{id}/validate"
     }
   };
 
-  // Apiary mock URLs
-  self.url = {
-    getTimesheetForUser: "http://private-696ecf-ddaugherfba.apiary-mock.com/api/timesheet"
-  };
   // self.requestHeaders.Prefer = "status=404"  // This is for mocked error responses with apiary to assist with manual testing
 
+  function replaceAll(targetString, replacementMapping){
+      var regex = new RegExp(Object.keys(replacementMapping).join("|"), "g");
 
-  // Prod URLs
-  // self.url = {
-  //   getTimesheetForUser: ""
-  // };
+      return targetString.replace(regex, function(matched) {
+          return replacementMapping[matched];
+      });
+  };
+
+  function generateURL(api, valueMap) {
+    return self.protocol + "://" + self.host + replaceAll(api, valueMap);
+  };
 
   self.fetchTimesheetInfo = function(username, date) {
+    var valueMap = {
+      "{email_shortname}": username,
+      "{date}": TimesheetUtil.formatDateYYYYMMDD(date)
+    };
+
     $.ajax({
-      url: self.url.getTimesheetForUser + '/' + username + '/' + TimesheetUtil.formatDateYYYYMMDD(date),
+      url: generateURL(self.api.getTimesheetForUser, valueMap),
       crossDomain: true,
       headers: self.requestHeaders
     }).done(function(data) {
@@ -30,6 +44,42 @@ var TimesheetCommunication = (function() {
       ResponseHandling.displayError(jqXHR);
     });
   };
+
+  self.saveTimesheet = function(timesheetId, hoursForTimesheetEntries) {
+    var valueMap = {
+      "{id}": timesheetId
+    };
+
+    $.ajax({
+      url: generateURL(self.url.saveTimesheet, valueMap),
+      method: 'POST',
+      crossDomain: true,
+      headers: self.requestHeaders
+    }).done(function(data) {
+      // do something
+    }).fail(function(jqXHR) {
+      ResponseHandling.makeErrorResponseVisible();
+      ResponseHandling.displayError(jqXHR);
+    });
+
+    self.validateTimesheet = function(timesheetId) {
+      var valueMap = {
+        "{id}": timesheetId
+      };
+
+      $.ajax({
+        url: generateURL(self.url.validateTimesheet, valueMap),
+        method: 'POST',
+        crossDomain: true,
+        headers: self.requestHeaders
+      }).done(function(data) {
+        // do something
+      }).fail(function(jqXHR) {
+        ResponseHandling.makeErrorResponseVisible();
+        ResponseHandling.displayError(jqXHR);
+      });
+    };
+  }
 
   return self;
 })();
